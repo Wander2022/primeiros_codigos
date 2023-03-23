b4dfe6e3a004af0b615893b07aa3119dd2384f60
Wanderson Oliveira <contato@zuptinformatica.com.br>
2023-03-23 18:22:16 -0300

    divisao

---
+++ Primeiros_codigos/src/Primeiroscodigos/divisao.java
@@ -0,0 +1,22 @@
+package Primeiroscodigos;
+
+import java.util.Scanner;
+
+public class divisao {
+
+	public static void main(String[] args) {
+		int num1, num2, resultado;
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println(" Digite um número: ");
+		num1 = scanner.nextInt();
+		
+		System.out.println(" Digite um número: ");
+		num2 = scanner.nextInt();
+		
+		resultado = num1 / num2;
+		System.out.println(" A multiplicação dos dois numeros é: " + resultado);
+
+	}
+
+}
